f469dd7949f2115b7046edbdca0c6a375b4ce867
Tommy <quangtrung54677@gmail.com>
2026-02-25 00:14:39 +0700

    variable

---
+++ variable/variable.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// // Declares Variable Message
+// let message;
+// //assign value into variable
+// message = "I Love javascript";
+// alert(message);
+// const pikachu = "pikapikachuuuu";
+// confirm(pikachu);
+
+/*
+    1. Working with variable
+*/
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+
+/*
+    2. Giving the right name 
+*/
+
+let ourPlanetName = "Earth";
+let currentVisitorName = "Tommy Shelby";
